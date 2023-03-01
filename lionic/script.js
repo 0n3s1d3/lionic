ecfb724a713adeb8cdc34e06f00be936c8bb1cf6
0n3s1d3 <qwertyulor@mail.ru>
2023-03-01 19:15:52 +0300

    added btnMore for articles block

--- lionic/script.js
+++ lionic/script.js
@@ -14,5 +14,14 @@ menuLinks.forEach(function (el) {
       menu.classList.remove('header__nav--active');
 
       document.body.classList.remove('stop-scroll')
-    })
-})
+    });
+});
+
+
+const btnMore = document.querySelector('.articles-more');
+const articlesItems = document.querySelectorAll('.articles-item');
+
+btnMore.addEventListener('click', () => {
+  articlesItems.forEach(el => { el.classList.add('articles-item--visible')});
+  btnMore.closest('.articles-center').classList.add('articles-center--hidden');
+});
